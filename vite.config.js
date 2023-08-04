42bc056d5af2444be5f46c1e85e04b0057a045f5
Jieun0123 <jieun.lee@benefitplus.kr>
2023-08-04 06:40:52 +0000

    [INITIAL] breeze, react

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,18 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
+            input: 'resources/js/app.jsx',
             refresh: true,
         }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
+        react(),
   ],
   server: {
-      hmr: {
-          host: 'localhost',
-      },
+    hmr: {
+      host: 'localhost',
+    },
   },
 });
