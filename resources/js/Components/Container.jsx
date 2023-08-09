d34bbba280a3aaf58471bfb29121aba5aee9260b
Dahye Jang <dhjang@benefitplus.kr>
2023-08-09 14:31:32 +0900

    Add layout and Card components

---
+++ resources/js/Components/Container.jsx
@@ -0,0 +1,9 @@
+import React, { Children } from "react";
+
+const Container = () => {
+    return (
+
+    );
+};
+
+export default Container;
---
+++ resources/js/Components/Search.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Search = () => {
+    return <div className="bg-blue_01 h-[60px]"></div>;
+};
+
+export default Search;
