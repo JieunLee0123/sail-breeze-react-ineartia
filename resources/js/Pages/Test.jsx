874cd3583b4cbac295d4d8c1be1b5f977fcb77dd
Dahye Jang <dhjang@benefitplus.kr>
2023-08-10 00:00:21 +0900

    Add Percentage bar

--- resources/js/Pages/Test.jsx
+++ resources/js/Pages/Test.jsx
@@ -8,23 +8,22 @@ import data from "../../../database/data.json";
 const Test = () => {
     return (
         <div className="font-pretendard">
-          <Header />
-      
-          <div className="mt-[30px] flex justify-center">
-            <div className="container px-4">
-              <div className="w-full lg:mx-0 py-8">
-                <Subject />
-                <div className="h-10" />
-                <Search />
-                <div className="h-20" />
-                <p>{data.length}개의 상품이 있습니다.</p>
-                <div className="h-5" />
-                <CardList data={data}/>
-              </div>
+            <Header />
+            <div className="mt-[30px] flex justify-center">
+                <div className="container px-4">
+                    <div className="w-full lg:mx-0 py-8">
+                        <Subject />
+                        <div className="h-10" />
+                        <Search data={data} />
+                        <div className="h-20" />
+                        <p>{data.length}개의 상품이 있습니다.</p>
+                        <div className="h-5" />
+                        <CardList data={data} />
+                    </div>
+                </div>
             </div>
-          </div>
-        
-          <Footer />
+
+            <Footer />
         </div>
     );
 };
